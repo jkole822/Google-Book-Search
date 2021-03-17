1a9f73a52abd691487120cf74fc65302514b5016
Kole Gasior <jkole822@gmail.com>
2021-03-17 13:10:29 -0400

    add results to saved comp

--- client/src/components/Saved.js
+++ client/src/components/Saved.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 
+import Result from "./Result";
+
 const Saved = () => {
-	return (
-		<Container>
-			<Typography>Saved</Typography>
-		</Container>
-	);
+	const [books, setBooks] = useState({});
+
+	useEffect(() => {
+		fetchSavedBooks();
+	}, []);
+
+	const fetchSavedBooks = async () => {
+		const fetchedBooks = await axios.get("/api/books");
+
+		setBooks(fetchedBooks.data);
+	};
+
+	const renderResults = () => {
+		if (books.length) {
+			return books.map(result => {
+				console.log(result);
+				return (
+					<Result
+						key={result._id}
+						info={result}
+						image={result.image}
+						link={result.link}
+					/>
+				);
+			});
+		}
+		return null;
+	};
+
+	return <Container>{renderResults()}</Container>;
 };
 
 export default Saved;
