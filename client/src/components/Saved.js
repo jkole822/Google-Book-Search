e6d5701d3f0ed3eb4ae74e04b534014eddebf117
Kole Gasior <jkole822@gmail.com>
2021-03-18 17:41:58 -0400

    adjust margin and padding

--- client/src/components/Saved.js
+++ client/src/components/Saved.js
@@ -1,9 +1,21 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
 import Result from "./Result";
+
+const useStyles = makeStyles({
+	savedHeader: {
+		marginBottom: 15,
+	},
+	container: {
+		border: "3px solid black",
+		borderRadius: 7,
+	},
+});
 
 const Saved = () => {
 	const [books, setBooks] = useState({});
@@ -11,6 +23,8 @@ const Saved = () => {
 	useEffect(() => {
 		fetchSavedBooks();
 	}, []);
+
+	const classes = useStyles();
 
 	const fetchSavedBooks = async () => {
 		const fetchedBooks = await axios.get("/api/books");
@@ -41,7 +55,16 @@ const Saved = () => {
 		return null;
 	};
 
-	return <Container>{renderResults()}</Container>;
+	return (
+		<Container>
+			<Box my={5} p={5} className={classes.container}>
+				<Typography variant="h5" component="h3" className={classes.savedHeader}>
+					Saved Books
+				</Typography>
+				<Box>{renderResults()}</Box>
+			</Box>
+		</Container>
+	);
 };
 
 export default Saved;
