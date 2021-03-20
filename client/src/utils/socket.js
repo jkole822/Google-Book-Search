b1d960613951b1d040fe7769d464546da83ee0d4
Kole Gasior <jkole822@gmail.com>
2021-03-20 11:22:06 -0400

    add test code for deployment

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -3,6 +3,7 @@ import { io } from "socket.io-client";
 let URL;
 
 if (process.env.NODE_ENV === "production") {
+	console.log("production");
 	URL = "https://jkg-google-books.herokuapp.com";
 } else {
 	URL = "http://localhost:5000";
