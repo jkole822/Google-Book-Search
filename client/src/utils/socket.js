4fde7e72655de37ae7a0fbdb4745fb689090c299
Kole Gasior <jkole822@gmail.com>
2021-03-20 15:00:37 -0400

    update Url and add comment

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
-// const URL = "https://jkg-google-books.herokuapp.com";
-const URL = 'http://localhost:5000'
+const URL = "https://jkg-google-books.herokuapp.com";
+// For Development:
+// const URL = 'http://localhost:5000'
 
 const socket = io(URL);
 
